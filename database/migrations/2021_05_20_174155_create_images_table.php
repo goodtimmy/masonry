ded1110f35068412d362fdb5fd548e9e79d470fd
tim-pronenko@yandex.ru <tim-pronenko@yandex.ru>
2021-05-20 21:29:00 +0400

    P1.1

---
+++ database/migrations/2021_05_20_174155_create_images_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('images', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_user')->nullable();
+            $table->string('real_name');
+            $table->string('stored_name');
+            $table->string('title')->nullable();
+            $table->text('description')->nullable();
+            $table->boolean('veryfied')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('images');
+    }
+}
